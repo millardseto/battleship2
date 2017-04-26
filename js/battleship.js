5c5a982c9fc393b6093747de72da7bbcfb2cec09
Millard Seto <millardseto@mac.com>
2017-04-26 11:16:23 -0700

    check user guess and indicate hit or miss

--- js/battleship.js
+++ js/battleship.js
@@ -1,41 +1,64 @@
-$(function(){
+$(function() {
+    // ship
+    var randomLoc = Math.floor(Math.random() * 5); // random 0 to 4
+    var location1 = randomLoc;
+    var location2 = location1 + 1;
+    var location3 = location2 + 1;
 
-  var guesses = 0;
-  var hits = 0;
 
-  $("#btnOK").click(makeBattleZone);
+    var guesses = 0;
+    var hits = 0;
 
-  function makeBattleZone() {
+    $("#btnOK").click(makeBattleZone);
 
-    var columns = $("#columns").val();
+    function makeBattleZone() {
 
-    /* remove table if it already exists */
-    $("#myTable").remove();
+        var columns = $("#columns").val();
 
-    var x = document.createElement("TABLE");
-    x.setAttribute("id", "myTable");
-    document.body.appendChild(x);
+        /* remove table if it already exists */
+        $("#myTable").remove();
 
-    var y = document.createElement("TR");
-    y.setAttribute("id", "myTr");
-    document.getElementById("myTable").appendChild(y);
+        var x = document.createElement("TABLE");
+        x.setAttribute("id", "myTable");
+        document.body.appendChild(x);
 
-    /* loop and create cells with unique ids */
-    for(i = 0; i < columns; i++) {
-      var z = document.createElement("TD");
-      z.setAttribute("id", i);
-      var t = document.createTextNode(i);
-      z.appendChild(t);
-      document.getElementById("myTr").appendChild(z);
+        var y = document.createElement("TR");
+        y.setAttribute("id", "myTr");
+        document.getElementById("myTable").appendChild(y);
+
+        /* loop and create cells with unique ids */
+        for (i = 0; i < columns; i++) {
+            var z = document.createElement("TD");
+            z.setAttribute("id", i);
+            var t = document.createTextNode(i);
+            z.appendChild(t);
+            document.getElementById("myTr").appendChild(z);
+        }
+
+        /* handle user guess */
+        $("td").click(function(td) {
+            //alert("Clicked: " + td.target.id);
+            var guess = td.target.id;
+
+            guesses++;
+
+            if (guess == location1 || guess == location2 || guess == location3) {
+                //alert("HIT!");
+
+                $(td.target).addClass("hit");
+                hits++;
+
+                if (hits == 3) {
+                    isSunk = true;
+                    alert("you sank my battleship!");
+                }
+            } else {
+                //alert("MISS");
+                $(td.target).addClass("miss");
+            }
+
+            $("#guesses").val(guesses);
+            $("#accuracy").val(hits / guesses);
+        });
     }
-
-    /* event handler */
-    $("td").click(function(td){
-      alert("Clicked: " + td.target.id);
-      guesses++;
-
-      $("#guesses").val(guesses);
-      $("#accuracy").val(hits/guesses);
-    });
-  }
 });
